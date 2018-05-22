bd8dbdd578fb6697b264cfb93c46e06188410745
joseandersonp <joseandersonp@outlook.com>
2018-05-22 15:53:07 -0300

    Traduzido tickers.go

--- examples/tickers/tickers.go
+++ examples/tickers/tickers.go
@@ -1,8 +1,7 @@
-// [Timers](timers) are for when you want to do
-// something once in the future - _tickers_ are for when
-// you want to do something repeatedly at regular
-// intervals. Here's an example of a ticker that ticks
-// periodically until we stop it.
+// [Timers](timers) são usados para fazer algo uma vez no
+// futuro - _tickers_ são usados para fazer algo repetidamente
+// em intervalos regulares de tempo. Aqui está um exemplo
+// de um ticker que executa periodicamente até pararmos.
 
 package main
 
@@ -11,21 +10,21 @@ import "fmt"
 
 func main() {
 
-    // Tickers use a similar mechanism to timers: a
-    // channel that is sent values. Here we'll use the
-    // `range` builtin on the channel to iterate over
-    // the values as they arrive every 500ms.
-    ticker := time.NewTicker(500 * time.Millisecond)
-    go func() {
-        for t := range ticker.C {
-            fmt.Println("Tick at", t)
-        }
-    }()
+	// Tickers usam um mecanismo semelhante aos timers: um
+	// um canal para o qual os valores são enviados. Aqui
+	// usaremos o `range` no canal para iterar os valores
+	// conforme eles chegam a cada 500 ms.
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick em", t)
+		}
+	}()
 
-    // Tickers can be stopped like timers. Once a ticker
-    // is stopped it won't receive any more values on its
-    // channel. We'll stop ours after 1600ms.
-    time.Sleep(1600 * time.Millisecond)
-    ticker.Stop()
-    fmt.Println("Ticker stopped")
+	// Tickers podem ser parados assim como os timers. Depois
+	// que um ticker é parado, ele não receberá mais valores em
+	// seu canal. Vamos parar o nosso depois de 1600ms.
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	fmt.Println("Ticker parado")
 }
--- examples/tickers/tickers.sh
+++ examples/tickers/tickers.sh
@@ -1,7 +1,7 @@
-# When we run this program the ticker should tick 3 times
-# before we stop it.
+# Quando executamos este programa, o ticker deve ser 
+# executado 3 vezes antes de ser interrompido.
 $ go run tickers.go
-Tick at 2012-09-23 11:29:56.487625 -0700 PDT
-Tick at 2012-09-23 11:29:56.988063 -0700 PDT
-Tick at 2012-09-23 11:29:57.488076 -0700 PDT
-Ticker stopped
+Tick em 2012-09-23 11:29:56.487625 -0700 PDT
+Tick em 2012-09-23 11:29:56.988063 -0700 PDT
+Tick em 2012-09-23 11:29:57.488076 -0700 PDT
+Ticker parado
